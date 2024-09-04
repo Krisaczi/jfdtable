f6fe87102fafaa4117370de7c5cb950412003f70
Krzysztof <krisaczi@yahoo.com>
2024-09-04 20:48:25 +0200

    table

--- src/store.tsx
+++ src/store.tsx
@@ -1,11 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './userSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
 
 const store = configureStore({
-    reducer: {
-        users: userReducer;
-    }
-})
+  reducer: {
+    users: userReducer,
+  },
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
